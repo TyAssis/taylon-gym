c27d2a6f339947d9950c1f15470f15953caf01b1
TyAssis <tyassis3@gmail.com>
2019-08-11 21:52:48 -0300

    Refactor by afonsof

--- mentoring/afonsof/sort-pure-functions/sortPureFunctions.js
+++ mentoring/afonsof/sort-pure-functions/sortPureFunctions.js
@@ -1,32 +1,53 @@
-let newList = [];
-
+// [3,2,1]
 const sortPureFunctions = (list) => {
-	let tempList = newList.length == 0 ? 
-		list : 
-		list.filter(function (element) {
-			let cnt = 0;
-			for (let i = 0; i < newList.length; i++) {
-				if(element > newList[i])
-					cnt++;
 
-				if(cnt == newList.length)
-					return element;
-			}
-		});
+	if (!Array.isArray(list))
+		return null;
 
-	let lesser = tempList[0];
+	if (list.length === 0)
+		return list;
+	
+	let pivot = list[0];
+	const leftList = [];
+	const rightList = [];
 
-	for (let i = 0; i < tempList.length; i++) {
-		if(tempList[i] <= lesser)
-			lesser = tempList[i];
+	for(let i = 1; i < list.length; i++) {
+		if (list[i] < pivot)
+			leftList.push(list[i]);
+		else 
+			rightList.push(list[i]);
 	}
 
-	list.forEach(function (element) {
-		if (element == lesser)
-			newList.push(element);
-	});
+	return [... sortPureFunctions(leftList), pivot, ...sortPureFunctions(rightList)];
 
-	return list.length == newList.length ? newList : sortPureFunctions(list);
+	// let tempList = newList.length === 0 ? 
+	// 	list : 
+	// 	list.filter((element) => {
+	// 		let cnt = 0;
+	// 		for (let i = 0; i < newList.length; i++) {
+	// 			if(element > newList[i])
+	// 				cnt++;
+
+	// 			if(cnt === newList.length)
+	// 				return element;
+	// 		}
+	// 	});
+
+	// let lesser = tempList[0];
+
+	// for (let i = 0; i < tempList.length; i++) {
+	// 	if(tempList[i] <= lesser)
+	// 		lesser = tempList[i];
+	// }
+
+	// const x = [... newList];
+
+	// list.forEach(function (element) {
+	// 	if (element === lesser)
+	// 		x.push(element);
+	// });
+
+	// return list.length === newList.length ? newList : sortPureFunctions(list, x);
 };
 
 module.exports = {
