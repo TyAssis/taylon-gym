0bd46d76e0d572aae85a3a6904229c9861c08cfe
TyAssis <tyassis3@gmail.com>
2019-08-18 22:26:25 -0300

    Inital gupy pricing refactor

---
+++ mentoring/afonsof/test/gupyPricingRefactored.test.js
@@ -0,0 +1,8 @@
+const { assert } = require('chai');
+const { gupyPricing } = require('../gupy-pricing/gupyPricingRefactored.js');
+
+describe('initial test gupyPricing', () => {
+    it('', () => {
+		assert.equal(gupyPricing(), '');
+	});
+});
