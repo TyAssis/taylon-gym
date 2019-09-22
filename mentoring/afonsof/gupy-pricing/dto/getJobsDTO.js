13c582dd8e11df5edcdd42bf729062789d21cc0b
TyAssis <tyassis3@gmail.com>
2019-09-22 19:24:24 -0300

    Refactor jobs reading for mongoose

--- mentoring/afonsof/gupy-pricing/dto/getJobsDTO.js
+++ mentoring/afonsof/gupy-pricing/dto/getJobsDTO.js
@@ -1,11 +1,7 @@
 const { getJobs } = require('../database/get-document.js');
 
 const getJobsDTO = async (job_ids) => {
-    const jobs = await getJobs(job_ids);
-    return jobs.reduce((acc, cur) => {
-        acc[cur._id] = { name: cur.name, type: cur.type}
-        return acc;
-    }, {});
+    return await getJobs(job_ids);
 };
 
 module.exports = getJobsDTO;
--- mentoring/afonsof/test/gupy-database.test.js
+++ mentoring/afonsof/test/gupy-database.test.js
@@ -1,6 +1,7 @@
 const { assert } = require('chai');
 const getCompanyDTO = require('../gupy-pricing/dto/getCompanyDTO.js');
 const getCompanyJobsDTO = require('../gupy-pricing/dto/getCompanyJobsDTO.js');
+const getJobsDTO = require('../gupy-pricing/dto/getJobsDTO.js');
 const mongoose = require('mongoose');
 
 describe('database query functions', () => {
@@ -9,14 +10,30 @@ describe('database query functions', () => {
         assert(company.subdomain, 'ambev');
     });
 
-    it('ambev ObjectId should return companyJobs documents', async () => {
+    it('ambev ObjectId should return ambev companyJobs documents', async () => {
         const company = await getCompanyDTO('ambev');
         const companyJobs = await getCompanyJobsDTO(company.id);
-        companyJobs.forEach((companyJob) => {
+        companyJobs.forEach(companyJob => {
             assert.equal(mongoose.Types.ObjectId.isValid(companyJob.companyId), true);
             assert.equal(mongoose.Types.ObjectId.isValid(companyJob.jobs.jobId), true);    
             assert(companyJob.companyId, company.id);
-            assert(company.subdomain, 'ambev');
         });
     });
+
+    it('ambev companyJobs should return ambev jobs', async () => {
+        const company = await getCompanyDTO('ambev');
+        const companyJobs = await getCompanyJobsDTO(company.id);
+        // companyJobs.forEach(companyJob => {
+        //     // Q: testar um basta? loop in loop?
+
+        // });
+        const jobsIds = companyJobs.map(companyJob => companyJob.jobs.jobId);
+        const jobs = await getJobsDTO(jobsIds);
+        const job = jobs[0];
+        assert.oneOf(job.type, ['effective', 'talentPool'], 'type is not in the list');
+        assert.typeOf(job.name, 'string');
+    });
 });
+
+
+// Q: comecei com R, deveria ter começado com C para testar tudo?!
